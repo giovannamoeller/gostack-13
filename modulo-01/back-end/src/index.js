61e9b9533e1b0037e52cf44b88d9444e980ef13b
giovannamoeller <giovannagmoeller@gmail.com>
2020-08-17 23:55:42 -0300

    Aplicação funcional

--- modulo-01/back-end/src/index.js
+++ modulo-01/back-end/src/index.js
@@ -1,41 +1,62 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 app.use(express.json());
 
+const projects = [];
+
 app.get('/projects', (req, res) => {
-    const { title, owner } = req.query;
-    return res.json([
-        'Projeto 1',
-        'Projeto 2'
-    ]);
+    const { title } = req.query;
+
+    const results = title ? 
+    projects.filter(project => project.title.includes(title)) 
+    : projects
+
+    return res.json(results);
 });
 
 app.post('/projects', (req, res) => {
     const { title, owner } = req.body;
-    return res.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+    const project = { id: uuid(), title, owner };
+    projects.push(project);
+    return res.json(project);
 });
 
 app.put('/projects/:id', (req, res) => {
     const { id } = req.params;
     const { title, owner } = req.body;
-    return res.json([
-        'Projeto 4',
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+    
+    const projectIndex = projects.findIndex(findProject => findProject.id === id);
+
+    const project = { id, title, owner };
+    projects[projectIndex] = project;
+
+    if(projectIndex < 0) { // se não encontrou o índice
+        return res.status(400).json({
+            error: "Couldn't update project"
+        });
+    } 
+
+    return res.json(project);
+
 });
 
 app.delete('/projects/:id', (req, res) => {
     const { id } = req.params;
-    return res.json([
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+
+    const projectIndex = projects.findIndex(findProject => findProject.id === id);
+
+    
+    if(projectIndex < 0) { // se não encontrou o índice
+        return res.status(400).json({
+            error: "Couldn't delete project"
+        });
+    } 
+
+    projects.splice(projectIndex, 1);
+
+    return res.status(204).send();
 });
 
 app.listen(3333, () => {
