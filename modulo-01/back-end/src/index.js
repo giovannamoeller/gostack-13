86f74ce4721583e76383db45c60be56080307355
giovannamoeller <giovannagmoeller@gmail.com>
2020-08-17 22:56:45 -0300

    Métodos HTTP

--- modulo-01/back-end/src/index.js
+++ modulo-01/back-end/src/index.js
@@ -9,6 +9,29 @@ app.get('/projects', (req, res) => {
     ]);
 });
 
+app.post('/projects', (req, res) => {
+    return res.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.put('/projects/:id', (req, res) => {
+    return res.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.delete('/projects/:id', (req, res) => {
+    return res.json([
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
 app.listen(3333, () => {
     console.log('🌸 Server is running');
 });
