913ae56af21a783553ee477eb44cdc5e20cea522
giovannamoeller <giovannagmoeller@gmail.com>
2020-08-19 16:57:06 -0300

    Configurando Babel

---
+++ modulo-01/front-end/babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    presets: [
+        '@babel/preset-env', // entende o ambiente que a aplicação está sendo executada
+        '@babel/preset-react'
+    ],
+}
---
+++ modulo-01/front-end/src/index.js
@@ -0,0 +1,5 @@
+const soma = (a, b) => {
+    return a + b;
+}
+
+console.log(2, 2);
