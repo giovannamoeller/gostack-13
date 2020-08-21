3f4a1dcfeaf17eb28251c13bf0582c57cadca29d
giovannamoeller <giovannagmoeller@gmail.com>
2020-08-20 21:07:46 -0300

    Iniciando com ReactNative

---
+++ modulo-01/mobile/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+    return (
+        <View>
+            <Text>Olá!</Text>
+        </View>
+    )
+}
+
+const styles = new StyleSheet.create({
+    
+})
