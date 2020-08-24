1ae11c596319412f3a823aa9ba19d8e0652a123a
giovannamoeller <giovannagmoeller@gmail.com>
2020-08-24 14:06:23 -0300

    Primeiro projeto com Node

--- modulo-02/projeto-node/src/server.ts
+++ modulo-02/projeto-node/src/server.ts
@@ -3,10 +3,8 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.send('Novo projeto');
-});
+app.get('/', (req, res) => res.send('Novo projeto'));
 
 app.listen(3333, () => {
-    console.log('👻 Server is running');
+  console.log('👻 Server is running');
 });
