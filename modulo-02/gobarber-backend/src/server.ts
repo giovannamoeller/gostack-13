f298b553d1ec561322cbad15db4c98e56544f1b4
giovannamoeller <giovannagmoeller@gmail.com>
2020-08-31 17:33:07 -0300

    Configurando os arquivos estáticos

--- modulo-02/gobarber-backend/src/server.ts
+++ modulo-02/gobarber-backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import routes from './routes';
 import "reflect-metadata";
+import uploadConfig from './config/upload';
 
 import './database';
 
 const app = express();
+app.use('/files', express.static(uploadConfig.directory));
 app.use(express.json());
 app.use(routes);
 
