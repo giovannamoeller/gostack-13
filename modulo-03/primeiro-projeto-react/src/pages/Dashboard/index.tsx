8ebd2b3263424ed4d93389b3498676711fa8578a
giovannamoeller <giovannagmoeller@gmail.com>
2020-09-04 15:27:40 -0300

    Finalizando estilização da página Dashboard

--- modulo-03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ modulo-03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,22 +1,56 @@
-import React from 'react';
-import { Title } from './styles';
-import logo from '../../assets/logo.svg';
+import React from "react";
+import { Title, Form, Repositories } from "./styles";
+import logo from "../../assets/logo.svg";
+import { FiChevronRight, FiChevronsRight } from "react-icons/fi";
 
-
-const Dashboard: React.FC = () => { // consegue definir a tipagem da função mais fácil
-    //React.FC = Function Component
-    return (
-        <div>
-            <img src={logo} alt="Github Explorer"/>
-            <Title>Explore repositórios no GitHub.</Title>
-            <div className="input-form">
-                <input type="text" placeholder="Digite aqui"/>
-                <button type="submit">Pesquisar</button>
-            </div>
-        </div>
-    )
-
-}
-
+const Dashboard: React.FC = () => {
+  // consegue definir a tipagem da função mais fácil
+  //React.FC = Function Component
+  return (
+    <div>
+      <img src={logo} alt="Github Explorer" />
+      <Title>Explore repositórios no GitHub.</Title>
+      <Form>
+        <input type="text" placeholder="Digite aqui" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/47362960?s=460&u=99702db3dedab50f47b0f151acea1e2e9db1b3fc&v=4"
+            alt="GitHub profile"
+          />
+          <div>
+            <strong>giovannamoeller/soundCloud-API</strong>
+            <p>Utilização da API do SoundCloud.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/47362960?s=460&u=99702db3dedab50f47b0f151acea1e2e9db1b3fc&v=4"
+            alt="GitHub profile"
+          />
+          <div>
+            <strong>giovannamoeller/giphy-API</strong>
+            <p>Utilização da API do Giphy.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/47362960?s=460&u=99702db3dedab50f47b0f151acea1e2e9db1b3fc&v=4"
+            alt="GitHub profile"
+          />
+          <div>
+            <strong>giovannamoeller/omnifood</strong>
+            <p>Site de delivery de comidas saudáveis.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </div>
+  );
+};
 
 export default Dashboard;
--- modulo-03/primeiro-projeto-react/src/pages/Dashboard/styles.ts
+++ modulo-03/primeiro-projeto-react/src/pages/Dashboard/styles.ts
@@ -1,13 +1,96 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Title = styled.h1`
-    font-size: 48px;
-    color: #3A3A3A;
-    margin-top: 10vh;
-    width: 450px;
-    line-height: 70px;
+  font-size: 48px;
+  color: #3a3a3a;
+  margin-top: 10vh;
+  max-width: 450px;
+  line-height: 60px;
 `;
 
+export const Form = styled.div`
+  margin-top: 5vh;
+
+  input {
+    padding: 15px 20px;
+    min-width: 400px;
+    border: none;
+    border-radius: 5px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    padding: 15px 20px;
+    min-width: 200px;
+    border: none;
+    border-radius: 8px 0;
+    background: #04d361;
+    border-radius: 0px 5px 5px 0px;
+    color: white;
+    cursor: pointer;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      filter: brightness(90%);
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 10vh;
+  max-width: 700px;
+
+  a {
+    background-color: white;
+    border-radius: 8px;
+    width: 100%;
+    padding: 16px 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+
+    transition: transform 0.2s;
+
+    & + a { /* & representa um elemento */
+      margin-top: 24px;
+    } /* precedido */
+
+    &:hover {
+      transform: translateX(10px); /* adiciona 10px no eixo X */
+    }
+  }
 
 
+  img {
+    width: 64px;
+    border-radius: 50%;
+  }
 
+  div {
+    margin-left: 20px;
+    line-height: 28px;
+    strong {
+      color: #3d3d4d;
+      font-size: 22px;
+    }
+
+    p {
+      color: #a8a8b3;
+      font-size: 18px;
+    }
+  }
+
+  svg {
+    color: #C9C9D4;
+    margin-left: auto;
+  }
+
+
+`;
