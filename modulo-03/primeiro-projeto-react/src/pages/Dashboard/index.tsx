60569ff55051e9e0689c16a926f162673619e0fa
giovannamoeller <giovannagmoeller@gmail.com>
2020-09-04 00:24:13 -0300

    Iniciando a estilização

--- modulo-03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ modulo-03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Title } from './styles';
+import logo from '../../assets/logo.svg';
 
 
 const Dashboard: React.FC = () => { // consegue definir a tipagem da função mais fácil
     //React.FC = Function Component
     return (
         <div>
+            <img src={logo} alt="Github Explorer"/>
             <Title>Explore repositórios no GitHub.</Title>
+            <div className="input-form">
+                <input type="text" placeholder="Digite aqui"/>
+                <button type="submit">Pesquisar</button>
+            </div>
         </div>
     )
 
--- modulo-03/primeiro-projeto-react/src/pages/Dashboard/styles.ts
+++ modulo-03/primeiro-projeto-react/src/pages/Dashboard/styles.ts
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 export const Title = styled.h1`
     font-size: 48px;
     color: #3A3A3A;
+    margin-top: 10vh;
+    width: 450px;
+    line-height: 70px;
 `;
+
+
 
 
