4b75f5d2d557686fac406609234e2da1865634e7
giovannamoeller <giovannagmoeller@gmail.com>
2020-09-13 14:47:47 -0300

    Utilizando unform nos formulários

--- modulo-03/gobarber-web/src/pages/SignUp/index.tsx
+++ modulo-03/gobarber-web/src/pages/SignUp/index.tsx
@@ -2,27 +2,33 @@ import React from 'react';
 import logo from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 import { FiMail, FiUser, FiLock, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp:React.FC = () => (
-    <Container>
+const SignUp:React.FC = () => {
+    function handleSubmit(data: Object) {
+        console.log(data)
+    }
+    return (
+        <Container>
         <Background/>
         <Content>
             <img src={logo} alt="GoBarber"/>
-            <form>
+            <Form onSubmit={handleSubmit}>
                 <h1>Faça seu cadastro</h1>
 
                 <Input name="name" type="text" icon={FiUser} placeholder="Nome"/>
                 <Input name="email" type="email" icon={FiMail} placeholder="E-mail"/>
                 <Input name="password" type="password" icon={FiLock} placeholder="Senha"/>
                 <Button type="submit">Cadastrar</Button>
-            </form>
+            </Form>
             <a href="#"><FiArrowLeft/>Voltar para o login</a>
         </Content>
                     
-    </Container>
-);
+        </Container>
+    );
+};
 
 export default SignUp;
