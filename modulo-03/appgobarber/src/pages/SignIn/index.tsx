657f4a643a4c8f0d9bcdffad8e1f5277bb2891b4
giovannamoeller <giovannagmoeller@gmail.com>
2020-09-25 00:46:29 -0300

    Importando imagens e arrumando problema da declaração de módulos

--- modulo-03/appgobarber/src/pages/SignIn/index.tsx
+++ modulo-03/appgobarber/src/pages/SignIn/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
+import { Image } from 'react-native';
+import logo from '../../assets/logo.png';
 
 import { Container } from './styles';
 
 const SignIn: React.FC = () => {
     return (
         <Container>
+            <Image source={logo}/>
         </Container>
     )
 }
