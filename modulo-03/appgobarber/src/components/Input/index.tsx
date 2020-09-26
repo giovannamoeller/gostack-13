db600ebe96a4e56cf3a3e4e5ed7bf77a0411533a
giovannamoeller <giovannagmoeller@gmail.com>
2020-09-25 23:25:21 -0300

    Fazendo foco e preenchimento de campos

--- modulo-03/appgobarber/src/components/Input/index.tsx
+++ modulo-03/appgobarber/src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useImperativeHandle, forwardRef } from "react";
+import React, { useEffect, useRef, useImperativeHandle, forwardRef, useState, useCallback } from "react";
 import { TextInputProps } from "react-native";
 import { useField } from '@unform/core';
 
@@ -21,6 +21,22 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({ name, ico
   const { registerField, defaultValue = '', error, fieldName } = useField(name);
   const inputValueRef = useRef<InputValueReference>({value: defaultValue});
   const inputElementRef = useRef<any>(null);
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur= useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputValueRef.current.value);
+
+  }, []);
+
+
   useImperativeHandle(ref, () => ({
     focus() {
       inputElementRef.current.focus();
@@ -35,14 +51,16 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({ name, ico
     })
   }, [fieldName, registerField]);
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360"/>
+    <Container isFocused={isFocused}>
+      <Icon name={icon} size={20} color={isFocused || isFilled ? '#FF9000' : '#666360'} />
       <TextInput {...rest} keyboardAppearance="dark"
       onChangeText={value => {
         inputValueRef.current.value = value;
       }} 
       ref={inputElementRef}
       defaultValue={defaultValue}
+      onFocus={handleInputFocus}
+      onBlur={handleInputBlur}
       />
     </Container>
   );
