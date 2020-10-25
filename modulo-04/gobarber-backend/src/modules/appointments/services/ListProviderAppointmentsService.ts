09b44cb9835924c32159ce490b073e1476e66a03
giovannamoeller <giovannagmoeller@gmail.com>
2020-10-25 13:16:32 -0300

    Cache da lista dos providers

--- modulo-04/gobarber-backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ modulo-04/gobarber-backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from 'tsyringe';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface Request {
     provider_id: string;
@@ -16,13 +17,19 @@ class ListProviderMonthAvailabilityService {
     
     constructor(
         @inject('AppointmentsRepository')
-        private appointmentsRepository: IAppointmentsRepository
+        private appointmentsRepository: IAppointmentsRepository,
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ){}
 
     public async execute({ provider_id, day, month, year }: Request): Promise<Appointment[]> {
+        const cacheData = await this.cacheProvider.recover('ask');
+        console.log(cacheData);
         const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
             provider_id, day, month, year
         });
+
+        await this.cacheProvider.save('ask', 'asd');
 
         return appointments;
     }
