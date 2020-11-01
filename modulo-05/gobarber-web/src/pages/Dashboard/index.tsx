0b4c9b595195937efb08e9324bbef43ee3df60b9
giovannamoeller <giovannagmoeller@gmail.com>
2020-11-01 19:39:05 -0300

    Estilizando o próximo agendamento

--- modulo-05/gobarber-web/src/pages/Dashboard/index.tsx
+++ modulo-05/gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,35 +1,70 @@
-import React from 'react';
+import React from "react";
 
-import { Container, Header, HeaderContent, Profile } from './styles';
-import logo from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
-import { useAuth } from '../../hooks/auth';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from "./styles";
+import logo from "../../assets/logo.svg";
+import { FiClock, FiPower } from "react-icons/fi";
+import { useAuth } from "../../hooks/auth";
 
+const DashBoard: React.FC = () => {
+  const { signOut, user } = useAuth();
 
-const DashBoard:React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logo} alt="GoBarber" />
+          <Profile>
+            {console.log(user.avatar_url)}
+            {user.avatar_url && <img src={user.avatar_url} alt={user.name} />}
+            <div>
+              <span>Bem vindo(a),</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
 
-    const { signOut, user } = useAuth();
+          <button type="button">
+            <FiPower onClick={signOut} />
+          </button>
+        </HeaderContent>
+      </Header>
 
-    return (
-        <Container>
-            <Header>
-                <HeaderContent>
-                    <img src={logo} alt="GoBarber"/>
-                    <Profile>
-                        <img src={user.avatar_url} alt={user.name}/>
-                        <div>
-                            <span>Bem vindo,</span>
-                            <strong>{user.name}</strong>
-                        </div>
-                    </Profile>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-Feira</span>
+          </p>
 
-                    <button type="button">
-                        <FiPower onClick={signOut}/>
-                    </button>
-                </HeaderContent>
-            </Header>
-        </Container>
-    )
-}
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars1.githubusercontent.com/u/47362960?s=460&u=99702db3dedab50f47b0f151acea1e2e9db1b3fc&v=4"
+                alt=""
+              />
+              <strong>Giovanna Moeller</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
+    </Container>
+  );
+};
 
 export default DashBoard;
--- modulo-05/gobarber-web/src/pages/Dashboard/styles.ts
+++ modulo-05/gobarber-web/src/pages/Dashboard/styles.ts
@@ -55,3 +55,97 @@ export const Profile = styled.div`
         }
     }
 `;
+
+export const Content = styled.main`
+    max-width: 1120px;
+    margin: 64px auto;
+    display: flex;
+`;
+
+export const Schedule = styled.div`
+    flex: 1; // ocupa todo espaço possível
+    margin-right: 120px;
+
+    h1 {
+        font-size: 36px;
+    }
+
+    p {
+        margin-top: 8px;
+        color: #FF9000;
+        display: flex;
+
+        span {
+            display: flex;
+            align-items: center;
+        }
+
+        span + span::before { // = do 2º span pra frente
+            content: '';
+            align-items: center;
+            width: 1px;
+            height: 12px;
+            background: #FF9000;
+            margin: 0 8px;
+        }
+    }
+
+`;
+
+export const NextAppointment = styled.div`
+    margin-top: 64px;
+
+    strong {
+        color: #999591;
+        font-size: 20px;
+        font-weight: 400;
+    }
+
+    div {
+        background: #3E3B47;
+        display: flex;
+        align-items: center;
+        padding: 16px 24px;
+        border-radius: 10px;
+        margin-top: 24px;
+        max-width: 640px;
+        position: relative;
+
+        &::before {
+            position: absolute;
+            content: '';
+            height: 80%;
+            width: 1px;
+            left: 0;
+            top: 10%;
+            background: #FF9000;
+        }
+
+        img {
+            width: 80px;
+            border-radius: 50%;
+            margin-right: 32px;
+        }
+
+        strong {
+            color: white;
+        }
+
+
+        span {
+            display: flex;
+            align-items: center;
+            margin-left: auto;
+            svg {
+                color: #FF9000;
+                margin-right: 8px;
+            }
+            color: #999591;
+        }
+    }
+
+`;
+
+export const Calendar = styled.aside`
+    max-width: 360px;
+`;
