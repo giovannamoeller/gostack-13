0d336c58def7635e2350745dccd91a262b3a251f
giovannamoeller <giovannagmoeller@gmail.com>
2020-11-03 13:17:38 -0300

    Ajustes da API para iniciar o aplicativo mobile

--- modulo-04/gobarber-backend/src/modules/appointments/infra/http/controllers/ProvidersControllers.ts
+++ modulo-04/gobarber-backend/src/modules/appointments/infra/http/controllers/ProvidersControllers.ts
@@ -2,6 +2,7 @@ import { container } from 'tsyringe';
 import { Request, Response } from 'express';
 
 import ListProvidersService from "@modules/appointments/services/ListProvidersService";
+import { classToClass } from 'class-transformer';
 
 class ProvidersControllers {
     public async index (request: Request, response: Response): Promise<Response> {
@@ -11,7 +12,7 @@ class ProvidersControllers {
 
         const providers = await listProviders.execute({ user_id });
 
-        return response.json(providers);
+        return response.json(classToClass(providers));
     }
 }
 
--- modulo-04/gobarber-backend/src/modules/appointments/services/ListProvidersService.ts
+++ modulo-04/gobarber-backend/src/modules/appointments/services/ListProvidersService.ts
@@ -4,6 +4,7 @@ import { inject, injectable } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 
 interface Request {
     user_id: string;
@@ -20,10 +21,9 @@ class ListProvidersService {
 
     public async execute({ user_id }: Request): Promise<User[]> {
         let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`);
-
         if(!users) {
             users = await this.usersRepository.findAllProviders({except_user_id: user_id});
-            await this.cacheProvider.save(`providers-list:${user_id}`, users)
+            await this.cacheProvider.save(`providers-list:${user_id}`, classToClass(users))
         }
 
         return users;
